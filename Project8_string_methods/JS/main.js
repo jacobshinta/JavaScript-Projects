d6478ea8ad4957135145e979ba9c23732234395d
jacobshinta <jacobshinta@me.com>
2021-03-05 13:48:06 -0500

    update comments project8

--- Project8_string_methods/JS/main.js
+++ Project8_string_methods/JS/main.js
@@ -1,47 +1,23 @@
-function full_Sentence() {
-    var part_1 = "I have made ";
+function full_Sentence() { //declare function
+    var part_1 = "I have made "; //declare variables
     var part_2 = "this into a ";
     var part_3 = "complete sentence.";
-    var whole_sentence = part_1.concat(part_2, part_3);
-    document.getElementById("concatenate").innerHTML = whole_sentence;
+    var whole_sentence = part_1.concat(part_2, part_3); //concatenate teh variable using concat method and declare result variable
+    document.getElementById("concatenate").innerHTML = whole_sentence; //display "whole_sentence" at specified ID
 }
 
-function slice_Method() {
-    var Sentence = "All work and no play makes Johnny a dull boy.";
-    var Section = Sentence.slice(27,33);
-    document.getElementById("Slice").innerHTML = Section;
+function slice_Method() { //declare function
+    var Sentence = "All work and no play makes Johnny a dull boy."; //apply variable to the string
+    var Section = Sentence.slice(27,33); //slice mthod at specified positions
+    document.getElementById("Slice").innerHTML = Section; //display "Section" at specified ID
 }
 
-function toUpper_Method() {
-    var string = "Uppercase now!";
-    var result = string.toUpperCase();
-    document.getElementById("Upper").innerHTML = result;
-  }
-
-function search_Method() {
-    var string = "Search String."; 
-    var n = string.search("String");
-    document.getElementById("search").innerHTML = n;
-  }
-
-function string_Method() {
-    var x = 182;
-    document.getElementById("Numbers_to_string").innerHTML = x.toString();
+function string_Method() { //declare function
+    var x = 182; //declare variable for the value
+    document.getElementById("Numbers_to_string").innerHTML = x.toString(); //use toString method to display number as a string at the specified ID
 }
 
-function precision_Method() {
-    var x = 15646.561656351;
-    document.getElementById("Precision").innerHTML = x.toPrecision(10);
+function precision_Method() { //declare function
+    var x = 15646.561656351; //declare variable for the value
+    document.getElementById("Precision").innerHTML = x.toPrecision(10); //use toPrecision method to round to the 10th position and display "x" at specified ID
 }
-
-function toFixed_Method() {
-    var number = 5.56789;
-    var x = number.toFixed(2);
-    document.getElementById("fixed").innerHTML = x;
-}
-
-function valueOf_Method() {
-    var string = "Value.";
-    var result = string.valueOf();
-    document.getElementById("valueOf").innerHTML = result;
-  }
