1f70c76043b7690e9aa7f3ac73b470822ec4a9a1
jacobshinta <jacobshinta@me.com>
2021-03-22 14:28:34 -0400

    Update Pizza.js

--- Pizza_Project/Pizza_JavaScript/Pizza.js
+++ Pizza_Project/Pizza_JavaScript/Pizza.js
@@ -1,15 +1,15 @@
-function getReceipt() {
+function getReceipt() { //define function and variables
     var text1 = "<h3>You Ordered:</h3>";
     var runningTotal = 0;
     var sizeTotal = 0;
     var sizeArray = document.getElementsByClassName("size");
-    for (var i = 0; i < sizeArray.length; i++) {
+    for (var i = 0; i < sizeArray.length; i++) { //loop that checks which selection was made and displays it
         if (sizeArray[i].checked) {
             var selectedSize = sizeArray[i].value;
             text1 = text1+selectedSize+"<br>";
         }
     }
-    if (selectedSize === "Personal Pizza") {
+    if (selectedSize === "Personal Pizza") { //set values to variables depending on selection
         sizeTotal = 6;
     } else if (selectedSize === "Small Pizza") {
         sizeTotal = 8;
@@ -18,31 +18,31 @@ function getReceipt() {
     } else if (selectedSize === "Large Pizza") {
         sizeTotal = 14;
     }
-    runningTotal = sizeTotal;
+    runningTotal = sizeTotal; //the following lines are to store the values
     console.log(selectedSize+" = $"+sizeTotal+".00");
     console.log("size text1: "+text1);
     console.log("subtotal: $"+runningTotal+".00");
     getTopping(runningTotal,text1);
 };
 
-function getTopping(runningTotal,text1) {
+function getTopping(runningTotal,text1) { //define function and set variables
     var toppingTotal = 0;
-    var selectedTopping = [];
+    var selectedTopping = []; //empty array to hold the selections
     var toppingArray = document.getElementsByClassName("toppings");
-    for (var j= 0; j < toppingArray.length; j++) {
+    for (var j= 0; j < toppingArray.length; j++) { //loop to see whcih toppings are selected
         if (toppingArray[j].checked) {
             selectedTopping.push(toppingArray[j].value);
             console.log("selected topping item: ("+toppingArray[j].value+")");
             text1 = text1+toppingArray[j].value+"<br>";
         }
     }
-    var toppingCount = selectedTopping.length;
-    if (toppingCount > 1) {
+    var toppingCount = selectedTopping.length; //checks the amount of toppings selected
+    if (toppingCount > 1) { //since a free topping is given this makes the count work
         toppingTotal = (toppingCount - 1);
     } else {
         toppingTotal = 0;
     }
-    runningTotal = (runningTotal + toppingTotal);
+    runningTotal = (runningTotal + toppingTotal); //adds the toppings to the previous total and displays the string and values stored throughout
     console.log("total selected topping items: "+toppingCount);
     console.log(toppingCount+" topping - 1 free topping = "+"$"+toppingTotal+".00");
     console.log("topping text1: "+text1);
